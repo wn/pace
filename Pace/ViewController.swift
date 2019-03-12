9f6ef821ab9afb1285affdd9dc139dd0a74c18cb
wn <weineng.a@gmail.com>
2019-03-12 23:31:44 +0800

    First commit

---
+++ Pace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 12/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
