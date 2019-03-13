69345e5cf0e97441c6c1eb823cfe7f929df2cea2
wn <weineng.a@gmail.com>
2019-03-13 13:34:38 +0800

    Test location

--- Pace/ViewController.swift
+++ Pace/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var location: UILabel!
+
+    let locationManager = CLLocationManager()
+
+    @IBAction func spawnLocation(_ sender: Any) {
+        locationManager(locationManager, didUpdateLocations: [])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        location.text = "locations = \(locValue.latitude) \(locValue.longitude)"
+    }
 }
 
