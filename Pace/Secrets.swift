d31a9f6b2785fc75bf648d21104b44e38ad3a0f4
wn <weineng.a@gmail.com>
2019-03-16 12:45:58 +0800

    Cleared cache

--- Pace/Secrets.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Secrets.swift
-//  Pace
-//
-//  Created by Ang Wei Neng on 16/3/19.
-//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
-//
-
-import Foundation
---
+++ Pods/Target Support Files/Pods-PaceTests/Pods-PaceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PaceTests : NSObject
+@end
+@implementation PodsDummy_Pods_PaceTests
+@end
