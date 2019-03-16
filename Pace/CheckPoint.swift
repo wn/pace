e25614f56167fbd56c483858ecb1d4cd9554ee1a
Zhang Yuntong <ang.unong@gmail.com>
2019-03-16 14:17:33 +0800

    Bootstrap models

---
+++ Pace/CheckPoint.swift
@@ -0,0 +1,16 @@
+//
+//  LocatonTime.swift
+//  Pace
+//
+//  Created by Yuntong Zhang on 16/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import Foundation
+
+struct CheckPoint {
+    private let location: Location
+    private let time: Double
+    private let actualDistance: Double
+    private let RouteDistance: Double?
+}
---
+++ Pace/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Pace
+//
+//  Created by Yuntong Zhang on 16/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import Foundation
+
+class User: Hashable {
+
+    private let id: Int
+
+    init(id: Int) {
+        self.id = id
+    }
+
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
