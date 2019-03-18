dbbb06799b432a3be087642953024d880937f91f
wn <weineng.a@gmail.com>
2019-03-18 22:14:10 +0800

    Clean cache

--- Pods/Target Support Files/Pods-Pace/Pods-Pace-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pace : NSObject
-@end
-@implementation PodsDummy_Pods_Pace
-@end
--- Pods/Target Support Files/Pods-PaceTests/Pods-PaceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PaceTests : NSObject
-@end
-@implementation PodsDummy_Pods_PaceTests
-@end
