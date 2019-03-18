63983b2cfe01f6adf33963d669e5332457ea7fc3
wn <weineng.a@gmail.com>
2019-03-19 00:07:13 +0800

    Add secrets.swift

---
+++ Pace/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 16/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import Foundation
+
+class Secrets {
+    static let mapsToken = ""
