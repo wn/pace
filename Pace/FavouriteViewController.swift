08b7061e91aeb7043e5d126f2403f62fe1f01edf
wn <weineng.a@gmail.com>
2019-03-21 15:51:17 +0800

    Scaffold view controllers

---
+++ Pace/FavouriteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavouriteViewController.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 21/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+}
