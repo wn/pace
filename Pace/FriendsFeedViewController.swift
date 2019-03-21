801e22d2f90923c7b8c238f909c770cab2392615
Ang Wei Neng <weineng.a@gmail.com>
2019-03-21 19:04:12 +0800

    Scaffold viewcontrollers (#10)
    * Scaffold view controllers
    
    * Lint and set storyboard tabs name
    
    * Add icons

---
+++ Pace/FriendsFeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsFeedViewController.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 21/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import UIKit
+
+class FriendsFeedViewController: UIViewController {
+
+}
