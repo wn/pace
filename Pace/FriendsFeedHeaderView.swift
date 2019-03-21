b3e0a1a21512904fa25e12edd378b0858d9b735f
wn <weineng.a@gmail.com>
2019-03-21 23:47:31 +0800

    Add header

---
+++ Pace/FriendsFeedHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsFeedHeaderView.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 21/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import UIKit
+
+class FriendsFeedHeaderView: UICollectionReusableView {
+    @IBOutlet var title: UILabel!
+
+}
--- Pace/FriendsFeedViewController.swift
+++ Pace/FriendsFeedViewController.swift
@@ -9,5 +9,82 @@
 import UIKit
 
 class FriendsFeedViewController: UIViewController {
+    // MARK: - Properties
+    private let feedIdentifier = "friendsFeedCell"
+    let friendsRoutes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+    let itemsPerRow = 1
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                             left: 20.0,
+                                             bottom: 50.0,
+                                             right: 20.0)
+}
 
+// MARK: - UICollectionViewDataSource
+extension FriendsFeedViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    // Tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return friendsRoutes.count
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+        ) -> UICollectionViewCell {
+        let cell = collectionView
+            .dequeueReusableCell(withReuseIdentifier: feedIdentifier, for: indexPath) as! FriendsFeedCollectionViewCell
+        cell.backgroundColor = .blue
+        // Configure the cell
+        return cell
+    }
+}
+
+// MARK: - Collection View Flow Layout Delegate
+extension FriendsFeedViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * CGFloat(itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / CGFloat(itemsPerRow)
+
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                                 viewForSupplementaryElementOfKind kind: String,
+                                 at indexPath: IndexPath) -> UICollectionReusableView {
+        // 1
+        switch kind {
+        // 2
+        case UICollectionView.elementKindSectionHeader:
+            // 3
+            guard
+                let headerView = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: kind,
+                    withReuseIdentifier: "\(FriendsFeedHeaderView.self)",
+                    for: indexPath) as? FriendsFeedHeaderView
+                else {
+                    fatalError("Invalid view type")
+            }
+
+            headerView.title.text = "LOL"
+            return headerView
+        default:
+            // 4
+            assert(false, "Invalid element type")
+        }
+    }
 }
