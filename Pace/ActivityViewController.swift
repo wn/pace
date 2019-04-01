144001b203b5ec95bd80428813f598b3a3b6fd2e
wn <weineng.a@gmail.com>
2019-04-02 01:28:41 +0800

    Poll live data

--- Pace/ActivityViewController.swift
+++ Pace/ActivityViewController.swift
@@ -11,10 +11,13 @@ import GoogleMaps
 import GooglePlaces
 import AVFoundation
 import DrawerKit
+import RealmSwift
 
 class ActivityViewController: UIViewController {
 
     var drawerDisplayController: DrawerDisplayController?
+
+    var routesManager = RealmRouteManager.forDefaultRealm
 
     @IBOutlet private var mapView: GMSMapView!
     // Keep track of all markers in the map
@@ -163,6 +166,20 @@ extension ActivityViewController: GMSMapViewDelegate {
 
         // TODO: Get all potential markers and generate them.
         // Get marker nearby location
+
+        // BELOW IS THE REAL CODE FOR GET NEARBY ROUTES.
+//        routesManager.getRoutesNear(location: location) { (routes, error) -> Void in
+//            self?.markers = [:]
+//            for index in 0..<routes.count {
+//                // Count is the number of routes that the marker represents
+//                // Set as 17 as that is the name of the image
+//                let count = 17
+//                let marker = self.generateRouteMarker(location: routes[index], count: count)
+//                self.markers[marker] = index
+//            }
+//        }
+
+        // TODO: STUB - TO REMOVE
         let routes = getNearbyRoutes()
         markers = [:]
         for index in 0..<routes.count {
@@ -181,72 +198,5 @@ extension ActivityViewController: GMSMapViewDelegate {
         mapView.setCameraPosition(location.coordinate)
         mapView.animate(toZoom: Constants.initialZoom)
         return true
-    }
-}
-
-private extension ActivityViewController {
-
-    // TODO: Automatically populate when marker is press, instead
-    // of pressing a button.
-    @IBAction func presentButtonTapped() {
-        doModalPresentation(passthrough: false)
-    }
-
-    func doModalPresentation(passthrough: Bool) {
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "presented")
-            as? PresentedViewController else { return }
-
-        // you can provide the configuration values in the initialiser...
-        var configuration = DrawerConfiguration(/* ..., ..., ..., */)
-
-        // ... or after initialisation. All of these have default values so change only
-        // what you need to configure differently. They're all listed here just so you
-        // can see what can be configured. The values listed are the default ones,
-        // except where indicated otherwise.
-        //        configuration.initialState = .collapsed
-        configuration.totalDurationInSeconds = 0.4
-        configuration.durationIsProportionalToDistanceTraveled = false
-        // default is UISpringTimingParameters()
-        configuration.timingCurveProvider = UISpringTimingParameters(dampingRatio: 0.8)
-        configuration.fullExpansionBehaviour = .coversFullScreen
-        configuration.supportsPartialExpansion = true
-        configuration.dismissesInStages = true
-        configuration.isDrawerDraggable = true
-        configuration.isFullyPresentableByDrawerTaps = true
-        configuration.numberOfTapsForFullDrawerPresentation = 1
-        configuration.isDismissableByOutsideDrawerTaps = true
-        configuration.numberOfTapsForOutsideDrawerDismissal = 1
-        configuration.flickSpeedThreshold = 3
-        configuration.upperMarkGap = 100 // default is 40
-        configuration.lowerMarkGap = 80 // default is 40
-        configuration.maximumCornerRadius = 15
-        configuration.cornerAnimationOption = .none
-        configuration.passthroughTouchesInStates = passthrough ? [.collapsed, .partiallyExpanded] : []
-
-        var handleViewConfiguration = HandleViewConfiguration()
-        handleViewConfiguration.autoAnimatesDimming = true
-        handleViewConfiguration.backgroundColor = .gray
-        handleViewConfiguration.size = CGSize(width: 40, height: 6)
-        handleViewConfiguration.top = 8
-        handleViewConfiguration.cornerRadius = .automatic
-        configuration.handleViewConfiguration = handleViewConfiguration
-
-        let borderColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
-        let drawerBorderConfiguration = DrawerBorderConfiguration(borderThickness: 0.5,
-                                                                  borderColor: borderColor)
-        configuration.drawerBorderConfiguration = drawerBorderConfiguration // default is nil
-
-        let drawerShadowConfiguration = DrawerShadowConfiguration(shadowOpacity: 0.75,
-                                                                  shadowRadius: 10,
-                                                                  shadowOffset: .zero,
-                                                                  shadowColor: .red)
-        configuration.drawerShadowConfiguration = drawerShadowConfiguration // default is nil
-
-        drawerDisplayController = DrawerDisplayController(presentingViewController: self,
-                                                          presentedViewController: vc,
-                                                          configuration: configuration,
-                                                          inDebugMode: true)
-
-        present(vc, animated: true)
     }
 }
