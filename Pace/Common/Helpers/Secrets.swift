81ca6ac840f34fec5adbc943655462da794e1be2
Tan Zheng Wei <tanzhengwei143@gmail.com>
2019-04-13 14:53:05 +0800

    Reorganized code

---
+++ Pace/Common/Helpers/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  Pace
+//
+//  Created by Ang Wei Neng on 16/3/19.
+//  Copyright © 2019 nus.cs3217.pace. All rights reserved.
+//
+
+import Foundation
+
+class Secrets {
+    static let mapsToken = "AIzaSyA-WYMMuNb0zW1O6A5TNft2OSYg8YEGcBY"
+}
