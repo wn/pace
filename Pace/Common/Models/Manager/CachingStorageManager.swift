412d06336399a816a0dda93b1ffef03d9ac34978
Julius S <jluuiss@gmail.com>
2019-04-15 17:51:03 +0800

    Reorganise protocols

--- Pace/Common/Models/Manager/CachingStorageManager.swift
+++ Pace/Common/Models/Manager/CachingStorageManager.swift
@@ -9,40 +9,6 @@
 import RealmSwift
 import CoreLocation
 
-protocol RealmStorageManager {
-    /// A Typealias for handling errors.
-    typealias ErrorHandler = (Error?) -> Void
-
-    /// The default (persistent) realm for this manager.
-    var persistentRealm: Realm { get }
-
-    /// The default in-memory realm for this manager.
-    var inMemoryRealm: Realm { get }
-
-    /// Attempts to fetch a route within this area.
-    func fetchRoutesWithin(latitudeMin: Double, latitudeMax: Double, longitudeMin: Double, longitudeMax: Double,
-                           _ errorHandler: @escaping ErrorHandler)
-
-    /// Attempts to fetch the runs for this specific Route.
-    /// - Precondition: `route` must exist in a realm.
-    func getRunsFor(route: Route)
-
-    /// Fetches runs but loads it into non-persistent memory
-    func getRunsFor(routeId: String)
-
-    /// Saves a new route.
-    func saveNewRoute(_ route: Route, _ completion: ErrorHandler?)
-
-    /// Saves a new run.
-    func saveNewRun(_ run: Run, toRoute: Route, _ completion: ErrorHandler?)
-
-    /// Adds a route to a user's favourites
-    func addFavouriteRoute(_ route: Route, toUser user: User, _ completion: ErrorHandler?)
-
-    /// Removes a route from a user
-    func removeFavouriteRoute(_ route: Route, fromUser user: User, _ completion: ErrorHandler?)
-}
-
 class CachingStorageManager: RealmStorageManager {
 
     /// The default RealmStorageManager
