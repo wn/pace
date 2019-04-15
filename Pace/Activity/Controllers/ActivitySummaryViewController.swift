d190ddf76b5085ac412675754630f1d0090c5358
wn <weineng.a@gmail.com>
2019-04-16 00:35:39 +0800

    Add save button to run summary

--- Pace/Activity/Controllers/ActivitySummaryViewController.swift
+++ Pace/Activity/Controllers/ActivitySummaryViewController.swift
@@ -10,23 +10,61 @@ import UIKit
 import CoreLocation
 
 class ActivitySummaryViewController: UIViewController {
+    // MARK: UI variables
     @IBOutlet private var distanceLabel: UILabel!
     @IBOutlet private var paceLabel: UILabel!
     @IBOutlet private var timeLabel: UILabel!
-    var createdRun: OngoingRun?
-    var routesManager: RealmStorageManager?
-    @IBOutlet var statsView: RunStatsView!
+    @IBOutlet private var statsView: RunStatsView!
 
-    @IBAction func saveRun(_ sender: UIButton) {
-        guard distance >= Constants.checkPointDistanceInterval else {
+    // MARK: Run variables
+    var createdRun: OngoingRun?
+    private var routesManager = CachingStorageManager.default
+    private var isSaved = false
+
+    // MARK: Run statistics variables
+    private var distance: Double = 0
+    private var pace: Int = 0
+    private var time: Double = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+        showStats()
+        readStats()
+    }
+
+    private func setupNavigation() {
+        navigationItem.title = Titles.runSummary
+        let image = UIImage(named: "save.png")?.withRenderingMode(.alwaysOriginal)
+        let saveButton = UIButton(type: .system)
+        let widthConstraint = saveButton.widthAnchor.constraint(equalToConstant: 30)
+        let heightConstraint = saveButton.heightAnchor.constraint(equalToConstant: 30)
+        widthConstraint.isActive = true
+        heightConstraint.isActive = true
+        saveButton.setImage(image, for: .normal)
+        saveButton.addTarget(self, action: #selector(popupSettings), for: .allTouchEvents)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButton)
+    }
+
+    @objc
+    private func popupSettings() {
+        guard !isSaved else {
+            return
+        }
+        saveRun()
+    }
+
+    private func saveRun() {
+        guard
+            distance >= Constants.checkPointDistanceInterval,
+            let route = createdRun?.toNewRoute()
+        else {
             print("CANT SAVE THIS SHIT cause distance not long enuff")
             return
         }
-        routesManager?.saveNewRoute(createdRun!.toNewRoute(), nil)
+        routesManager.saveNewRoute(route, nil)
+        print("RUN SAVED")
     }
-    var distance: Double = 0
-    var pace: Int = 0
-    var time: Double = 0
 
     func setStats(createdRun: OngoingRun, distance: CLLocationDistance, time: Double) {
         self.distance = distance
@@ -35,15 +73,11 @@ class ActivitySummaryViewController: UIViewController {
         self.createdRun = createdRun
     }
 
-    func showStats() {
+    private func showStats() {
         statsView.setStats(distance: distance, time: time)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = Titles.runSummary
-        routesManager = CachingStorageManager.default
-        showStats()
+    private func readStats() {
         VoiceAssistant.say("Distance: \(Int(distance)) meters")
         VoiceAssistant.say("Duration: \(Int(time)) seconds")
         VoiceAssistant.say("Pace: \(Int(pace)) seconds per kilometer")
